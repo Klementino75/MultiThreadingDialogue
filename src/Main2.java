b88150f9eb59605ccd5536e0476126f4c93ba6b5
Klementino75 <klementino75@ya.ru>
2024-10-05 00:28:22 +0300

    Added files

--- src/Main2.java
+++ src/Main2.java
@@ -1,13 +1,49 @@
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
 
 public class Main2 {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
 
-        // Создаем пул из 4 потоков фиксированного размера.
+        // Создаем пул потоков фиксированного размера.
         System.out.println("Создаю потоки...");
-        final ExecutorService pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        final ExecutorService threadPool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
+        // Создаем задачу с результатом типа String.
+        Callable<Integer> myCallableID1 = new MyCallable("Поток 1");
+        Callable<Integer> myCallableID2 = new MyCallable("Поток 2");
+        Callable<Integer> myCallableID3 = new MyCallable("Поток 3");
+        Callable<Integer> myCallableID4 = new MyCallable("Поток 4");
+
+        // Отправляем задачу на выполнение в пул потоков.
+        final Future<Integer> task1 = threadPool.submit(myCallableID1);
+        final Future<Integer> task2 = threadPool.submit(myCallableID2);
+        final Future<Integer> task3 = threadPool.submit(myCallableID3);
+        final Future<Integer> task4 = threadPool.submit(myCallableID4);
+
+        // Получаем результат.
+        final int resultOfTask1 = task1.get();
+        final int resultOfTask2 = task2.get();
+        final int resultOfTask3 = task3.get();
+        final int resultOfTask4 = task4.get();
+
+        // Выводим результат.
+        System.out.println("Поток 1, сообщений: " + resultOfTask1 + ".");
+        System.out.println("Поток 2, сообщений: " + resultOfTask2 + ".");
+        System.out.println("Поток 3, сообщений: " + resultOfTask3 + ".");
+        System.out.println("Поток 4, сообщений: " + resultOfTask4 + ".");
+
+        List<Callable<Integer>> tasks = new ArrayList<>();
+        tasks.add(myCallableID1);
+        tasks.add(myCallableID2);
+        tasks.add(myCallableID3);
+        tasks.add(myCallableID4);
+
+        System.out.println("\nРезультат метода invokeAny: " + threadPool.invokeAny(tasks));
+
+        // Завершаем работу пула потоков.
+        System.out.println("Завершаю все потоки.");
+        threadPool.shutdown();
     }
 }
--- src/MyCallable.java
+++ src/MyCallable.java
@@ -1,2 +1,26 @@
-public class MyCallable {
+import java.util.concurrent.Callable;
+
+public class MyCallable implements Callable<Integer> {
+
+    private final String name;
+    public MyCallable(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        int countMSG = 0; // количествово сообщений
+        try {
+            for (int i = 0; i <= 4; i++) {
+                Thread.sleep(2500);
+                System.out.println("Я, " + name + ". Всем привет!");
+                countMSG++;
+            }
+        } catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            System.out.printf("%s завершен.\n", name);
+        }
+        return countMSG;
+    }
 }
